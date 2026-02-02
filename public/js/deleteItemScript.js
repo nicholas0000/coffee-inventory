dcd8080b50fccd6101479dabdb1aabd4b6d6dda4
Henry <83106787+henrylin03@users.noreply.github.com>
2026-02-02 15:21:50 +1100

    Revert "feat: user can delete category after confirmation (#97)" (#98)
    This reverts commit f9abd8e845d5a53af896c202de869c3d4b326c0c.

--- public/js/deleteItemScript.js
+++ public/js/deleteItemScript.js
@@ -1,6 +1,13 @@
-const deleteButton = document.querySelector(".delete-btn");
+const deleteButtons = document.querySelectorAll(".delete-btn");
 const modal = document.querySelector("dialog");
 const modalCancelButton = modal.querySelector(".cancel-btn");
 
-deleteButton.addEventListener("click", () => modal.showModal());
+const showDeleteConfirmationModal = () => {
+	modal.showModal();
+};
+
+// attach event listeners
+[...deleteButtons].forEach((deleteBtn) => {
+	deleteBtn.addEventListener("click", showDeleteConfirmationModal);
+});
 modalCancelButton.addEventListener("click", () => modal.close());
