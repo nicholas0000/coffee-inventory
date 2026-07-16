be4cc038b460e89aa66132d705b167fd2ec09b4f
Nichoals  Tse <59206190+nicholas0000@users.noreply.github.com>
2026-07-16 19:07:15 +0100

    Refactor database connection to use DATABASE_URL

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 const { Pool } = require("pg");
-const { DATABASE_NAME } = require("./constants");
 
 module.exports = new Pool({
-	host: "localhost",
-	user: process.env.USER_NAME,
-	database: DATABASE_NAME,
-	password: process.env.PASSWORD,
-	port: 5432,
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
